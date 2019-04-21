7e69338acedfe3c1468207f4cbf9943036a68dd7
baron <owenashby@hotmail.nl>
2019-04-21 19:46:32 +0200

    Added Number_Wizard_UI

---
+++ Number_Wizard_UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        CalculateGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        if ((guess + 1) <= max)
+        {
+            min = guess + 1;
+            CalculateGuess();
+        }
+    }
+
+    public void OnPressLower()
+    {
+        if ((guess - 1) >= min)
+        {
+            max = guess - 1;
+            CalculateGuess();
+        }
+    }
+
+    void CalculateGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
